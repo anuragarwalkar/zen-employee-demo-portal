f0d32031401beaa822b8dc6850cc0678cf11ac14
Anurag Arwalkar <anuragarwalkar@gmail.com>
2019-02-15 13:36:34 +0530

    added router auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,12 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 const routes: Routes = [
   {path:'', component:LoginComponent},
-  {path:'Nav-bar', component:NavBarComponent},
-  {path:'Sign-up', component:SignUpComponent},
-  {path:'Emp-form', component: EmpFormComponent},
-  {path:'Zen-home', component:ZenHomeComponent},
-  {path:'Submited-forms', component:SubmitedFormsComponent},
-  {path:'EditUser/:id', component: EditUserComponent}
+  {path:'Nav-bar', component:NavBarComponent, canActivate:[AuthGuard]},
+  {path:'Sign-up', component:SignUpComponent,  canActivate:[AuthGuard]},
+  {path:'Emp-form', component: EmpFormComponent, canActivate:[AuthGuard]},
+  {path:'Zen-home', component:ZenHomeComponent, canActivate:[AuthGuard]},
+  {path:'Submited-forms', component:SubmitedFormsComponent, canActivate:[AuthGuard]},
+  {path:'EditUser/:id', component: EditUserComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
